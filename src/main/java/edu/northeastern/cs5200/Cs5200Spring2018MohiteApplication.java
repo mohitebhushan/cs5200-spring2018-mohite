ac16694ad0eb9c916b019d872d05d44f0d79a935
mohitebhushan <bhushan29.mohite@gmail.com>
2018-01-19 23:13:00 -0500

    First Commit

---
+++ src/main/java/edu/northeastern/cs5200/Cs5200Spring2018MohiteApplication.java
@@ -0,0 +1,13 @@
+package edu.northeastern.cs5200;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class Cs5200Spring2018MohiteApplication extends SpringBootServletInitializer{
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cs5200Spring2018MohiteApplication.class, args);
+	}
+}
